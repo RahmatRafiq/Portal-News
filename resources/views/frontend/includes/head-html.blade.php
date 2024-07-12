a847b8acc166e7dc1439a5e33c729884f58e50f7
RahmatRafiq <rahmatrafiq.1999@gmail.com>
2024-07-12 12:17:34 +0800

    chore: Update Open Graph metadata and page description for Persmalibratum.com UAJM

--- resources/views/frontend/includes/head-html.blade.php
+++ resources/views/frontend/includes/head-html.blade.php
@@ -1,6 +1,10 @@
-{{-- <!-- Required meta tags -->
+<!-- Required meta tags -->
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+<!-- Additional Meta Tags for SEO -->
+<meta name="description" content="Persmalibratum adalah portal berita mahasiswa Universitas Atmajaya Makassar yang menyediakan berita terkini dan informasi terbaru." />
+<meta name="keywords" content="berita, mahasiswa, universitas, portal berita, informasi terbaru" />
+<meta name="author" content="Persmalibratum UAJM" />
 <title>
     Persmalibratum.com UAJM
 </title>
@@ -16,36 +20,5 @@
 <link href="{{ asset('frontend/css/style_1.css') }}" rel="stylesheet" type="text/css" />
 <!-- Modernizr JS -->
 <script src="{{ asset('frontend/js/modernizr-3.5.0.min.js') }}"></script>
-<link rel="icon" href="{{ asset('backend/img/icon.ico') }}" type="image/x-icon" /> --}}
+<link rel="icon" href="{{ asset('backend/img/icon.ico') }}" type="image/x-icon" />
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Persmalibratum.com UAJM</title>
-
-    <!-- Open Graph Meta Tags -->
-  
-
-    <!-- Additional Meta Tags for SEO -->
-    <meta name="description" content="Persmalibratum adalah portal berita mahasiswa Universitas Atmajaya Makassar yang menyediakan berita terkini dan informasi terbaru." />
-    <meta name="keywords" content="berita, mahasiswa, universitas, portal berita, informasi terbaru" />
-    <meta name="author" content="Persmalibratum UAJM" />
-
-    <link href="{{ asset('frontend/css/media_query.css') }}" rel="stylesheet" type="text/css" />
-    <link href="{{ asset('frontend/css/bootstrap.css') }}" rel="stylesheet" type="text/css" />
-    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"
-        integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous" />
-    <link href="{{ asset('frontend/css/animate.css') }}" rel="stylesheet" type="text/css" />
-    <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet" />
-    <link href="{{ asset('frontend/css/owl.carousel.css') }}" rel="stylesheet" type="text/css" />
-    <link href="{{ asset('frontend/css/owl.theme.default.css') }}" rel="stylesheet" type="text/css" />
-    <!-- Bootstrap CSS -->
-    <link href="{{ asset('frontend/css/style_1.css') }}" rel="stylesheet" type="text/css" />
-    <!-- Modernizr JS -->
-    <script src="{{ asset('frontend/js/modernizr-3.5.0.min.js') }}"></script>
-    <link rel="icon" href="{{ asset('backend/img/icon.ico') }}" type="image/x-icon" />
-</head>
-
-</html>
